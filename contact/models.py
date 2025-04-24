16d59450f7d9ce1dfd3a5b48696c3adf2479b87f
Vitor Barbon <dev.vitorbarbon@gmail.com>
2025-04-24 00:08:14 -0300

    edit model contact

--- contact/models.py
+++ contact/models.py
@@ -6,6 +6,9 @@ class Contact(models.Model):
     firts_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
-    phone = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f'{self.firts_name} {self.last_name}'
